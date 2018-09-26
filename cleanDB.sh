8b744c9c8f510a849331acd9416c37e7e5550ee6
Dobloq <dobloq@gmail.com>
2018-09-26 17:36:44 +0200

    Cambios

---
+++ cleanDB.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+python3 manage.py makemigrations
+python3 manage.py migrate
+python3 manage.py createsuperuser
+echo 'dobloq'
+echo 'dobloq@gmail.com'
+echo 'eachtime3'
+echo 'eachtime3'
